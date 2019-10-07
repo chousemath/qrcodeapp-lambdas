74bf7f73463bce43b57b1c020ea784a8984f9ce1
Joseph Sungpil Choi <chousemath@gmail.com>
2019-10-07 18:35:56 +0900

    Compress files, and upload to S3

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const {
   _error, 
   _timestamp, 
 } = require('./libraries/common');
+const paramsLocations = { TableName: config.tableNames.locations };
+const paramsScans = { TableName: config.tableNames.scans };
 
 AWS.config.update({ region: config.region });
 
@@ -19,20 +21,31 @@ exports.createQRCodeLocation = async (event) => {
   let body;
   let statusCode;
   try {
-    const paramsRead = { TableName: config.tableNames.locations, Key: event, };
+    const paramsRead = Object.assign({ Key: event }, paramsLocations);
     const qrCodeLocation = await ddb.get(paramsRead).promise();
     if (qrCodeLocation && qrCodeLocation.Item) {
       statusCode = 400;
       body = _error(new Error('Location already exists'));
       return { statusCode, body };
     }
-    const params = { 
-      TableName: config.tableNames.locations, 
-      Item: Object.assign(event, { timestamp: _timestamp() }), 
-    };
+    const paramsRead = Object.assign({ Item: Object.assign(event, { timestamp: _timestamp() })}, paramsLocations);
     await ddb.put(params).promise();
     statusCode = 200;
     body = { ok: true };
+  } catch(e) {
+    statusCode = 500;
+    body = _error(e);
+  }
+  return { statusCode, body };
+};
+
+exports.readQRCodeLocations = async (event) => {
+  let body;
+  let statusCode;
+  try {
+    const qrCodeLocations = await ddb.scan(paramsLocations).promise();
+    statusCode = 200;
+    body = _data(qrCodeScans.Items.sort((a, b) => b.timestamp - a.timestamp));
   } catch(e) {
     statusCode = 500;
     body = _error(e);
@@ -44,7 +57,7 @@ exports.readQRCodeLocation = async (event) => {
   let body;
   let statusCode;
   try {
-    const params = { TableName: config.tableNames.locations, Key: event, };
+    const params = Object.assign({ Key: event }, paramsLocations);
     const qrCodeLocation = await ddb.get(params).promise();
     statusCode = 200;
     body = _data(qrCodeLocation.Item);
