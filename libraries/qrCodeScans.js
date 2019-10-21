a33187478fc0026e04fb551a3e69998144acc025
Joseph Sungpil Choi <chousemath@gmail.com>
2019-10-21 14:46:16 +0900

    Compress files, and upload to S3

--- libraries/qrCodeScans.js
+++ libraries/qrCodeScans.js
@@ -1,24 +1,24 @@
 const makeQRCodeScanID = category => {
-  return {
-    S: `${category}x${Math.floor(Math.random() * 10000000)}`,
-  };
+    return {
+        S: `${category}x${Math.floor(Math.random() * 10000000)}`,
+    };
 };
 exports.idForQRCodeScan = category =>
-  `${category}x${Math.floor(Math.random() * 10000000)}`;
+    `${category}x${Math.floor(Math.random() * 10000000)}`;
 exports.makeQRCodeScan = event => {
-  return {
-    id: makeQRCodeScanID(event.category),
-    code: { S: event.code },
-    location: { S: event.location },
-    longitude: { N: event.longitude.toString() },
-    latitude: { N: event.latitude.toString() },
-    category: { N: event.category.toString() },
-    timestamp: { N: new Date().getTime().toString() },
-  };
+    return {
+        id: makeQRCodeScanID(event.category),
+        code: { S: event.code },
+        location: { S: event.location },
+        longitude: { N: event.longitude.toString() },
+        latitude: { N: event.latitude.toString() },
+        category: { N: event.category.toString() },
+        timestamp: { N: new Date().getTime().toString() },
+    };
 };
 exports.makeQRCodeScanKey = event => {
-  return {
-    id: { S: event.id },
-    timestamp: { N: event.timestamp.toString() },
-  };
+    return {
+        id: { S: event.id },
+        timestamp: { N: event.timestamp.toString() },
+    };
 };
--- responses.js
+++ responses.js
@@ -1,34 +1,34 @@
 const headers = {
-  'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': '*',
 };
 exports.notFound = (data = 'Requested record could not be found.') => {
-  return {
-    statusCode: 404,
-    headers,
-    body: { ok: false, data },
-  };
+    return {
+        statusCode: 404,
+        headers,
+        body: { ok: false, data },
+    };
 };
 exports.ok = {
-  statusCode: 200,
-  headers,
-  body: { ok: true },
-};
-exports.notOK = {
-  statusCode: 500,
-  headers,
-  body: { ok: false },
-};
-exports.okWithData = data => {
-  return {
     statusCode: 200,
     headers,
-    body: { ok: true, data },
-  };
+    body: { ok: true },
 };
-exports.notOKWithData = data => {
-  return {
+exports.notOK = {
     statusCode: 500,
     headers,
-    body: { ok: false, data },
-  };
+    body: { ok: false },
+};
+exports.okWithData = data => {
+    return {
+        statusCode: 200,
+        headers,
+        body: { ok: true, data },
+    };
+};
+exports.notOKWithData = data => {
+    return {
+        statusCode: 500,
+        headers,
+        body: { ok: false, data },
+    };
 };
